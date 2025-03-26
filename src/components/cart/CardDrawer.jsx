113726e2e4ae784de11fabe49210972cac8235ed
Đặng Hoàng Đức <ducdanghoang96@gmail.com>
2025-03-26 15:29:54 +0700

    update cart

--- src/components/cart/CardDrawer.jsx
+++ src/components/cart/CardDrawer.jsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import CartIcon from "./cart_icon";
-import { Button, Drawer, DrawerContent, DrawerHeader } from "@heroui/react";
+import { Button, Drawer, DrawerBody, DrawerContent, DrawerFooter, DrawerHeader } from "@heroui/react";
 
 const CardDrawer = () => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -12,11 +12,20 @@ const CardDrawer = () => {
 			<Button isIconOnly variant="light" onPress={() => setIsOpen(true)} className="w-fit " size="sm">
 				<CartIcon />
 			</Button>
-			<Drawer isOpen={isOpen} onOpenChange={setIsOpen} shouldBlockScroll={false}>
+			<Drawer isOpen={isOpen} size="lg" onOpenChange={setIsOpen} shouldBlockScroll={false}>
 				<DrawerContent>
 					<DrawerHeader>
 						<h3 className="text-lg font-bold">Giỏ Hàng</h3>
 					</DrawerHeader>
+					<DrawerBody>
+						<h4>Hiện không có sản phẩm nào trong giỏ hàng</h4>
+					</DrawerBody>
+					<DrawerFooter>
+						<Button color="danger" variant="light" onPress={() => setIsOpen(false)}>
+							Close
+						</Button>
+						<Button color="primary">Action</Button>
+					</DrawerFooter>
 				</DrawerContent>
 			</Drawer>
 		</div>
